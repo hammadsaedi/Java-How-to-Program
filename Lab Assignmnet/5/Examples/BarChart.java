3722a5677bc16329fed50c102202b81be03cecec
Hammad Saaedi <71373419+hammadsaedi@users.noreply.github.com>
2022-04-11 21:31:04 +0500

    وہ تو جیسے اسی فراق میں تھا

--- Lab Assignmnet/5/Examples/BarChart.java
+++ Lab Assignmnet/5/Examples/BarChart.java
@@ -1,20 +1,17 @@
-import javax.sound.midi.Soundbank;
-
 // Written by: Hammad Saeedi (@hammadsaedi everywhere)
-// Date and Time: 11 April, 2022 5:35 pm
+// Date and Time: 11 April, 2022 8:50 pm
 // Book: Java How to Program, Late Objects, Global Edition
 // java 17.0.2 2022-01-18 LTS
 // Java(TM) SE Runtime Environment (build 17.0.2+8-LTS-86)
 // Java HotSpot(TM) 64-Bit Server VM (build 17.0.2+8-LTS-86, mixed mode, sharing)
 
 /** 
- *6.5.2 Using an Array Initializer (Fig. 6.3)
+ * 6.5.5 Using Bar Charts to Display Array Data Graphically (Fig. 6.6)
 */
 
 public class BarChart {
     // main method
     public static void main(String[] args) {
-        // initializer list specifies the initial value for each element
         int[] array = {0, 0, 0, 0, 0, 0, 1, 2, 4, 2, 1};
 
         System.out.println("Grade distribution:");
@@ -29,14 +26,11 @@ public class BarChart {
             }
 
             // print bar of astrict
-        }
+            for (int star = 0; star < array[counter]; star++){
+                System.out.print("*"); 
+            }
 
-        // column headings
-        System.out.printf("%s%8s%n", "Index", "Value");  // % [flags] [width] [.precision] conversion-character
-
-        // output each array element's value
-        for (int counter = 0; counter < array.length; counter++){  
-            System.out.printf("%5d%8d%n",counter, array[counter]);
+            System.out.println();
         }
     }
 }
---
+++ Lab Assignmnet/5/Examples/RollDie.java
@@ -0,0 +1,31 @@
+// Written by: Hammad Saeedi (@hammadsaedi everywhere)
+// Date and Time: 11 April, 2022 9:00 pm
+// Book: Java How to Program, Late Objects, Global Edition
+// java 17.0.2 2022-01-18 LTS
+// Java(TM) SE Runtime Environment (build 17.0.2+8-LTS-86)
+// Java HotSpot(TM) 64-Bit Server VM (build 17.0.2+8-LTS-86, mixed mode, sharing)
+
+/** 
+ * 6.5.6 Using the Elements of an Array as Counters (Fig. 6.7)
+*/
+
+import java.security.SecureRandom;
+
+public class RollDie {
+    public static void main(String[] args) {
+        SecureRandom randomNumbers = new SecureRandom();
+        int[] frequency = new int[7]; // array of frequency counters
+
+        // roll die 60,000,000 times; use die value as frequency index
+        for (int roll = 1; roll <= 60000000; roll++){
+            ++frequency[1 + randomNumbers.nextInt(6)];
+        }
+
+        System.out.printf("%s%10s%n", "Face", "Frequency");
+
+        // output each array element's value
+        for (int face = 1; face < frequency.length; face++){  
+            System.out.printf("%4d%10d%n",face, frequency[face]);
+        }
+    }
+}
